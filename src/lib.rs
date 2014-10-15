0b52ae08335405689ab1eef600ad3470f10bd531
Austin Bonander <austin.bonander@gmail.com>
2014-10-14 23:18:17 -0700

    Implement and test vector serialization

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,44 @@ pub enum APIError {
 }
 
 macro_rules! rest(
-    ($verb:ident $url:expr: fn $fn_name:ident () -> $ret:ident) => (
+    ($verb_ext: ident $url:expr: fn $fn_name:ident ($(?$param:ident: $ty: ty),+ $($arg:ident: $ty:ty),+) -> $ret:ty) => (
+        fn $fn_name($($arg: $ty),+) -> Result<$ret, APIError> {
+            use rest_client::RestClient;
+            use serialize::json::decode;
+
+            pair!()
+
+            let url = format!($url, $($param),+);
+
+            let owned = [$((stringify!($arg), $arg.to_string())),+];
+            let params: Vec<(&str, &str)> = owned.iter().map(pair).collect();
+            
+            let response = try!(RestClient::$verb_ext(url.as_slice(), params.as_slice()).map_err(|e| RestErr(e)));
+            
+            if response.body.is_empty() {
+                Err(StatusErr(response.status.to_string()))    
+            } else {
+                decode::<$ret>(response.body.as_slice()).map_err(|e| JsonErr(e))    
+            }
+        }
+    );
+    ($verb:ident $url:expr: fn $fn_name:ident ($(?$param:ident: $ty:ty),+) -> $ret:ty) => (
+        fn $fn_name($($param: $ty),+) -> Result<$ret, APIError> {
+            use rest_client::RestClient;
+            use serialize::json::decode;
+            
+            let url = format!($url, $($param),+);
+            
+            let response = try!(RestClient::$verb(url.as_slice()).map_err(|e| RestErr(e)));
+            
+            if response.body.is_empty() {
+                Err(StatusErr(response.status.to_string()))  
+            } else {
+                decode::<$ret>(response.body.as_slice()).map_err(|e| JsonErr(e))
+            }                
+        }
+    );
+    ($verb:ident $url:expr: fn $fn_name:ident () -> $ret:ty) => (
         fn $fn_name() -> Result<$ret, APIError> {
             use rest_client::RestClient;
             use serialize::json::decode;
@@ -30,7 +67,7 @@ macro_rules! rest(
             }                
         }
     );
-    ($verb_ext:ident $url:expr: fn $fn_name:ident ($($arg:ident: $ty:ty),+) -> $ret:ident) => (
+    ($verb_ext:ident $url:expr: fn $fn_name:ident ($($arg:ident: $ty:ty),+) -> $ret:ty) => (
         fn $fn_name($($arg: $ty),+) -> Result<$ret, APIError> {
             use rest_client::RestClient;
             use serialize::json::decode;
@@ -51,43 +88,7 @@ macro_rules! rest(
             }      
         }
     );
-    ($verb:ident $url:expr{$($param:tt),+}: fn $fn_name:ident () -> $ret:ident) => (
-        fn $fn_name() -> Result<$ret, APIError> {
-            use rest_client::RestClient;
-            use serialize::json::decode;
-            
-            let url = format!($url, $($param),+);
-            
-            let response = try!(RestClient::$verb(url.as_slice()).map_err(|e| RestErr(e)));
-            
-            if response.body.is_empty() {
-                Err(StatusErr(response.status.to_string()))  
-            } else {
-                decode::<$ret>(response.body.as_slice()).map_err(|e| JsonErr(e))
-            }                
-        }
-    );
-    ($verb_ext: ident $url:expr{$($param:tt),+}: fn $fn_name:ident ($($arg:ident: $ty:ty),+) -> $ret:ident) => (
-        fn $fn_name($($arg: $ty),+) -> Result<$ret, APIError> {
-            use rest_client::RestClient;
-            use serialize::json::decode;
-
-            pair!()
-
-            let url = format!($url, $($param),+);
-
-            let owned = [$((stringify!($arg), $arg.to_string())),+];
-            let params: Vec<(&str, &str)> = owned.iter().map(pair).collect();
-            
-            let response = try!(RestClient::$verb_ext(url.as_slice(), params.as_slice()).map_err(|e| RestErr(e)));
-            
-            if response.body.is_empty() {
-                Err(StatusErr(response.status.to_string()))    
-            } else {
-                decode::<$ret>(response.body.as_slice()).map_err(|e| JsonErr(e))    
-            }
-        }
-    )
+    
 )
 
 macro_rules! pair(
@@ -101,18 +102,18 @@ macro_rules! pair(
 
 #[macro_export]
 macro_rules! get(
-    ($url:expr: fn $fn_name:ident () -> $ret:ident) => (
+    ($url:expr: fn $fn_name:ident () -> $ret:ty) => (
         rest!(get $url: fn $fn_name() -> $ret)
     );
-    ($url:expr: fn $fn_name:ident ($($arg:ident: $ty:ty),*) -> $ret:ident) => (
+    ($url:expr: fn $fn_name:ident ($(?$param:ident: $ty: ty),+) -> $ret:ty) => (
+        rest!(get $url: fn $fn_name($(?$param: $ty),+) -> $ret)
+    );
+    ($url:expr: fn $fn_name:ident ($(?$param:ident: $ty: ty),+ $($arg:ident: $ty:ty),+) -> $ret:ty) => (
+        rest!(get_with_params $url: fn $fn_name($(?$param: $ty),+ $($arg: $ty),+) -> $ret)
+    );
+    ($url:expr: fn $fn_name:ident ($($arg:ident: $ty:ty),*) -> $ret:ty) => (
         rest!(get_with_params $url: fn $fn_name($($arg: $ty),+) -> $ret)
-    );
-    ($url:expr{$($param:tt),+}: fn $fn_name:ident () -> $ret:ident) => (
-        rest!(get $url{$($param),+}: fn $fn_name() -> $ret)
-    );
-    ($url:expr{$($param:tt),+}: fn $fn_name:ident ($($arg:ident: $ty:ty),+) -> $ret:ident) => (
-        rest!(get_with_params $url{$($param),+}: fn $fn_name($($arg: $ty),+) -> $ret)
-    );         
+    );             
 )
 
 
@@ -124,11 +125,13 @@ mod test{
     struct Test {
         hello: String,    
     }
-    
-    get!("https://raw.githubusercontent.com/cybergeek94/ferrite/master/json/hello_world.json": fn hello_world() -> Test)
    
-    // This probably won't work because the endpoint is down 
-    get!("http://echo.jsontest.com/hello/{}"{val}: fn hello(val: &str) -> Test)
+    // TODO: Host these on GitHub-Pages or a local test server 
+    get!("https://raw.githubusercontent.com/cybergeek94/ferrite/master/json/hello_world.json": fn hello_world() -> Test)
+  
+    get!("https://raw.githubusercontent.com/cybergeek94/ferrite/master/json/hello_vec.json": fn hello_vec() -> Vec<Test>)
+   
+    get!("https://raw.githubusercontent.com/cybergeek94/ferrite/master/json/hello_{}.json": fn hello(?val: &str) -> Test)
     
     #[test]
     fn test_hello_world() {
@@ -142,6 +145,18 @@ mod test{
         let test = hello("world").unwrap();
         
         assert!(test.hello.as_slice() == "world");    
+    }
+
+    #[test]
+    fn test_hello_vec() {
+        let test_vec = hello_vec().unwrap();
+        let mut test_iter = test_vec.iter();
+
+        assert!(test_iter.next().unwrap().hello.as_slice() == "world");
+        assert!(test_iter.next().unwrap().hello.as_slice() == "nation");
+        assert!(test_iter.next().unwrap().hello.as_slice() == "city");
+        assert!(test_iter.next().unwrap().hello.as_slice() == "person");
+        assert!(test_iter.next().is_none());              
     }
     
 }
